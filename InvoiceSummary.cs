b31402223447cf04ee7a0b861ec93c4fe9de4805
pravinc3798 <pravin.choudhary88@gmail.com>
2022-09-20 16:31:10 +0530

    Added InvoiceSummary class

---
+++ InvoiceSummary.cs
@@ -0,0 +1,29 @@
+﻿namespace CabInvoiceGenerator
+{
+    public class InvoiceSummary
+    {
+        public double TotalFare;
+        public int NumberOfRides;
+        public double AverageFare;
+
+        public InvoiceSummary(double totalFare, int numberOfRides)
+        {
+            TotalFare = totalFare;
+            NumberOfRides = numberOfRides;
+            AverageFare = totalFare / numberOfRides;
+        }
+
+        public override bool Equals(Object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj is not InvoiceSummary)
+                return false;
+
+            InvoiceSummary other = (InvoiceSummary)obj;
+
+            return NumberOfRides == other.NumberOfRides && TotalFare == other.TotalFare && AverageFare == other.AverageFare; 
+        }
+    }
+}
