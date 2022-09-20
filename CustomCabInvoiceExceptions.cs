4188bd8dfc0a601c6fd1947b91d8822574f960a0
pravinc3798 <pravin.choudhary88@gmail.com>
2022-09-20 16:30:59 +0530

    Added CustomCabInvoiceExceptions class

---
+++ CustomCabInvoiceExceptions.cs
@@ -0,0 +1,18 @@
+﻿namespace CabInvoiceGenerator
+{
+    public class CustomCabInvoiceExceptions : Exception
+    {
+        public enum CustomExceptions
+        {
+            INVALID_DISTANCE_EXCEPTION,
+            INVALID_TIME_EXCEPTION
+        }
+
+        public CustomExceptions Ex;
+
+        public CustomCabInvoiceExceptions(CustomExceptions ex, string message) : base(message)
+        {
+            Ex = ex;
+        }
+    }
+}
